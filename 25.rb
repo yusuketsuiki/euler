deab1041223ef76cd669997177e5a36590b63092
yusuketsuiki <yusuketsuiki@gmail.com>
2013-02-03 21:07:28 +0900

    25

---
+++ 25.rb
@@ -0,0 +1,19 @@
+i = 0
+num = 1
+fibonacci_list = []
+
+while 1
+	if i > 1
+		num = fibonacci_list[i - 1] + fibonacci_list[i -2]
+	end
+	
+	if num.to_s().length >= 1000
+		break
+	end
+
+	fibonacci_list << num
+	i += 1
+end
+
+p fibonacci_list.size()
+#p fibonacci_list
